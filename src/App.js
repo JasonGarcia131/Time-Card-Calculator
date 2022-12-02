844c6effae81c925f1df0ed425e8a69ab82c8500
Jason Garcia <jaygarcia0530@gmail.com>
2022-12-01 22:09:17 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import TimeCard from './components/TimeCard';
+import { useState } from 'react';
 
 function App() {
+
+  const [weeklyHours, setWeeklyHours] = useState([])
+
+  const sumWithInitial = weeklyHours.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Deluxity's Time Card Calculator</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Day</th>
+            <th>Time-In</th>
+            <th>Time-Out</th>
+            <th>Deduction {`(min)`}</th>
+            <th></th>
+            <th>Hours</th>
+          </tr>
+        </thead>
+        <tbody>
+          <TimeCard day="Monday" setWeeklyHours={setWeeklyHours} />
+          <TimeCard day="Tuesday" setWeeklyHours={setWeeklyHours} />
+          <TimeCard day="Wednesday" setWeeklyHours={setWeeklyHours} />
+          <TimeCard day="Thursday" setWeeklyHours={setWeeklyHours} />
+          <TimeCard day="Friday" setWeeklyHours={setWeeklyHours} />
+          <TimeCard day="Saturday" setWeeklyHours={setWeeklyHours} />
+          <TimeCard day="Sunday" setWeeklyHours={setWeeklyHours} />
+          <tr >
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+            <b>Total Weekly Hours:</b>
+            </td>
+          <td>
+             <b>{sumWithInitial}</b>
+          </td>
+        </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/components/TimeCard.jsx
@@ -0,0 +1,118 @@
+import { useState } from "react"
+
+function TimeCard(props) {
+
+    const { setWeeklyHours } = props
+
+    let inMinutesToHours;
+    let outMinutesToHours;
+    let inHours;
+    let outHours;
+    let hoursWorked;
+    let deduction;
+    
+    //State variable for tracking hours input
+    const [hours, setHours] = useState({
+        inHours: 0,
+        inMinutes: 0,
+        outHours: 0,
+        outMinutes: 0,
+        inTimeOfDay: "",
+        outTimeOfDay: "",
+        lunchDeduction: 0
+    })
+
+    //State variable for total hours worked and error message pop up.
+    const [totalHours, setTotalHours] = useState(0);
+    const [errorMessage, setErrorMessage] = useState("");
+
+    //Watches the changes from the input
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setHours({ ...hours, [name]: value })
+    }
+
+    //Calculates total hours
+    const calculate = () => {
+        //Sets error state variable to empty string
+        setErrorMessage("");
+
+        //Minutes to hours conversions
+        inMinutesToHours = hours.inMinutes / 60
+        outMinutesToHours = hours.outMinutes / 60
+        inHours = Number(hours.inHours) + inMinutesToHours;
+        outHours = Number(hours.outHours) + outMinutesToHours;
+        deduction = Number(hours.lunchDeduction) / 60;
+
+        //Case where the time of days are equal to each other. PM == PM or AM == AM
+        if (hours.inTimeOfDay === hours.outTimeOfDay) {
+            if (Number(hours.inHours) === 12) {
+                hoursWorked = outHours - inMinutesToHours - deduction
+                setWeeklyHours((prevData) => [...prevData, hoursWorked])
+                return setTotalHours(hoursWorked)
+
+            } else if (Number(hours.outHours) < Number(hours.inHours)) {
+                return setErrorMessage("Please double check times.")
+            } else if (Number(hours.outHours) === 12) {
+                return setErrorMessage("Please double check times.")
+            } else {
+                hoursWorked = outHours - inHours - deduction
+                setWeeklyHours((prevData) => [...prevData, hoursWorked])
+                return setTotalHours(hoursWorked)
+            }
+        }
+        //Case where time of day are differnt   
+        else {
+            if (Number(hours.outHours) === 12) {
+                hoursWorked = (12 - inHours) + outMinutesToHours - deduction
+                setWeeklyHours((prevData) => [...prevData, hoursWorked])
+                return setTotalHours(hoursWorked)
+            } else {
+                hoursWorked = (12 - inHours) + outHours - deduction
+                setWeeklyHours((prevData) => [...prevData, hoursWorked])
+                return setTotalHours(hoursWorked)
+            }
+
+        }
+    }
+
+    return (
+        <tr>
+            <td>
+                {props.day}
+            </td>
+            <td>
+                <div>
+                    <input type="number" name="inHours" value={hours.inHours} onChange={handleChange} /> :
+                    <input type="number" name="inMinutes" value={hours.inMinutes} onChange={handleChange} />
+                    <select name="inTimeOfDay" value={hours.inTimeOfDay} onChange={handleChange}>
+                        <option defaultValue={hours.inTimeOfDay}>AM</option>
+                        <option>PM</option>
+                    </select>
+                </div>
+            </td>
+            <td>
+                <div>
+                    <input type="number" name="outHours" value={hours.outHours} onChange={handleChange} /> :
+                    <input type="number" name="outMinutes" value={hours.outMinutes} onChange={handleChange} />
+                    <select name="outTimeOfDay" value={hours.outTimeOfDay} onChange={handleChange}>
+                        <option defaultValue={hours.inTimeOfDay}>AM</option>
+                        <option>PM</option>
+                    </select>
+                </div>
+            </td>
+            <td>
+                <input type="number" name="lunchDeduction" value={hours.lunchDeduction} onChange={handleChange} />
+            </td>
+            <td>
+                <button onClick={calculate}>Calculate</button>
+            </td>
+            <td>
+                {errorMessage.length > 0 ? <p style={{color: "red"}}>{errorMessage}</p>: totalHours}
+            </td>
+        </tr>
+
+    )
+}
+
+export default TimeCard
